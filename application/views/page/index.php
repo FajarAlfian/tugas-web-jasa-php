c0c11f361eb777b0f45a78264f88445975bfb952
FajarAlfian <99306931+FajarAlfian@users.noreply.github.com>
2022-10-09 20:22:32 +0700

    Add files via upload

---
+++ application/views/page/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Landing Page</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- icon -->
+	<link rel="icon" type="image/png" href="">
+	<!-- fonts -->
+	<!-- <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet"> -->
+	<!-- fontawesome -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<!-- css file -->
+	<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/hams-dashboard.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-white shadow sticky-top">
+		<div class="container">
+			<a class="navbar-brand" href="#"> Landing Page</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+
+				<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-daftar poppins me-md-2" type="button">Daftar</button>
+                    <button onclick="location.href='<?php echo site_url('login') ?>'" class="btn btn-login poppins" type="button">Login</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+			</div>
+		</div>
+	</nav>
+
+	<div class="landing-1">
+		<div class="jumbotron">
+			<div class="container">
+				<div class="bg-text">
+					<p class="display-2"> Bersama Kami</p>
+					<p class="display-4"> Dapatkan Website Terbaik Anda Sekarang </p>
+					<p class="lead"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+					<div class="box-square">
+						<div class="row" id="box">
+							<div class="col-md-6">
+								<p class="lead"> Lihat Paket </p>
+							</div>
+							<div class="col-md-6">
+								<p class="lead"> Promo 50%</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-2">
+		<div class="container">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+				<div class="row">
+					<div class="col-xl-6 col-lg-6 col-sm-12">
+						<p class="display-3 font-weight-bold"> About Us</p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+						<button class="btn btn-primary mb-2"> Read More</button>
+					</div>
+					<div class="col-xl-6 col-lg-6 col-sm-12">
+						<img src="assets/img/jenis-website.png" class="img-responsive">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-3">
+		<div class="container">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+				<div class="row">
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<img src="assets/img/web.jpg" class="img-responsive" id="img-web">
+					</div>
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<p class="display-3"> kenapa harus kami? karena..</p>
+						<div class="list">
+							<div class="lead"><i class="fas fa-check-circle" style="color: #1bff1b"></i> Harga Terjangkau</div>
+							<div class="lead"><i class="fas fa-check-circle" style="color: #1bff1b"></i> Dikerjakan Oleh Professional</div>
+							<div class="lead"><i class="fas fa-check-circle" style="color: #1bff1b"></i> Maintenance Selamanya</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="line"></div>
+
+	<div class="landing-4">
+		<div class="container">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+				<p class="display-3"> Pilihan Paket </p>
+				<div class="row">
+					<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+						<img src="assets/img/services/basic.png" class="img-responsive">
+						<p class="display-4"> BASIC </p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+						<button class="btn btn-primary"> Read More</button>
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+						<img src="assets/img/services/gold.webp" class="img-responsive">
+						<p class="display-4"> GOLD </p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+						<button class="btn btn-primary"> Read More</button>
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+						<img src="assets/img/services/premium.png" class="img-responsive">
+						<p class="display-4"> PREMIUM</p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+						<button class="btn btn-primary"> Read More</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-5">
+		<div class="jumbotron">
+			<p class="display-3 text-white"> Testimonials</p>
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<div class="card">
+							<div class="card-body">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.
+							</div>
+							<div class="card-body">
+									<img src="assets/img/face/face-1.png" class="img-responsive rounded-circle" style="margin-top: 2px;">
+									<p class="lead" style="margin-top: 1px;">Fajar</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-6">
+		<div class="container">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+				<div class="row">
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<p class="display-3"> Contact Us</p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+						<form>
+							<div class="form-group">
+								<input type="text" name="name" placeholder="Name" class="form-control">
+							</div>
+							<div class="form-group">
+								<input type="text" name="name" placeholder="Your Email" class="form-control">
+							</div>	
+							<div class="form-group">
+								<input type="text" name="name" placeholder="Message" class="form-control">
+							</div>
+							<button class="btn btn-primary btn-block"> SUBMIT</button>
+						</form>
+					</div>
+					<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<div class="con-text">
+							<p class="display-4"> Connect With US</p>
+							<p class="lead"> Email Addres <font style="color: #57e5ff;"> fajarweb@info.com</font></p>
+							<p class="display-4"> Number Phone</p>
+							<p class="lead"> Number <font style="color: #57e5ff;"> +62 8129902</font></p>
+							<p class="display-4"> Address</p>
+							<p class="lead"> Addres <font style="color: #57e5ff;"> Jl.Ciempeuy</font></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
